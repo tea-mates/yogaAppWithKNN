76aa0cdc0bb68112127da322a176998f738092f1
Sandra <sandrajanineo@gmail.com>
2019-05-06 16:21:46 -0400

    applied css to the about us page

--- client/components/AboutUs.js
+++ client/components/AboutUs.js
@@ -2,47 +2,91 @@ import React from 'react';
 
 const AboutUs = () => {
   return (
-    <div>
-      <div>Contact Us</div>
-      <div className="row">
-        <div className="align-center">
-          Janavi
-          <div className="col col-sm-2">
-            <img
-              className="aboutUsImage"
-              src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
-            />
+    <section className="section-secondary">
+      <h1 className="h2">About Us</h1>
+      <div className="row align-center">
+        <div className="col col-md-6">
+          <div className="card">
+            <h3 class="card-title">Janavi</h3>
+            <div>
+              <img
+                className="aboutUsImage"
+                src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
+              />
+            </div>
+            This is an example of a card. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Est quasi ipsam sint alias, nemo.
+            Dolore minus aliquid, modi debitis adipisci vel amet, sit officia
+            placeat?
+            <ul className="card-actions">
+              <li>
+                <button className="button-primary">Only 1 Action</button>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="align-center">
-          Sandra
-          <div className="col col-sm-2">
-            <img
-              className="aboutUsImage"
-              src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
-            />
+        <div className="col col-md-6">
+          <div className="card">
+            <h3 class="card-title">Lily</h3>
+            <div>
+              <img
+                className="aboutUsImage"
+                src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
+              />
+            </div>
+            This is an example of a card. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Est quasi ipsam sint alias, nemo.
+            Dolore minus aliquid, modi debitis adipisci vel amet, sit officia
+            placeat?
+            <ul className="card-actions">
+              <li>
+                <button className="button-primary">Only 1 Action</button>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="align-center">
-          Lily
-          <div className="col col-sm-2">
-            <img
-              className="aboutUsImage"
-              src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
-            />
+        <div className="col col-md-6">
+          <div className="card">
+            <h3 class="card-title">Priyanka</h3>
+            <div>
+              <img
+                className="aboutUsImage"
+                src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
+              />
+            </div>
+            This is an example of a card. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Est quasi ipsam sint alias, nemo.
+            Dolore minus aliquid, modi debitis adipisci vel amet, sit officia
+            placeat?
+            <ul className="card-actions">
+              <li>
+                <button className="button-primary">Only 1 Action</button>
+              </li>
+            </ul>
           </div>
         </div>
-        <div className="align-center">
-          Priyanka
-          <div className="col col-sm-2">
-            <img
-              className="aboutUsImage"
-              src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
-            />
+        <div className="col col-md-6">
+          <div className="card">
+            <h3 class="card-title">Sandra</h3>
+            <div>
+              <img
+                className="aboutUsImage"
+                src="https://cdn.shopify.com/s/files/1/1061/1924/products/Sunglasses_Emoji_be26cc0a-eef9-49e5-8da2-169bb417cc0b_large.png?v=1480481057"
+              />
+            </div>
+            This is an example of a card. Lorem ipsum dolor sit amet,
+            consectetur adipisicing elit. Est quasi ipsam sint alias, nemo.
+            Dolore minus aliquid, modi debitis adipisci vel amet, sit officia
+            placeat?
+            <ul className="card-actions">
+              <li>
+                <button className="button-primary">Only 1 Action</button>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
