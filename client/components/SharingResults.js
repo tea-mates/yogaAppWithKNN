ecaa8ecb3dba7fd6b7a8bcbdc495636f43710164
Lily Zdansky <lzdansky@mail.bradley.edu>
2019-05-08 16:11:29 -0400

    circular sharing results component ready

--- client/components/SharingResults.js
+++ client/components/SharingResults.js
@@ -5,23 +5,19 @@ export default class SharingResults extends React.Component {
     super(props);
   }
 
+  componentDidMount() {
+    const script = document.createElement("script");
+
+    script.src = `//platform-api.sharethis.com/js/sharethis.js#property=5cd32b213f59c700126bad69&product=sticky-share-buttons`;
+    script.async = true;
+
+    document.body.appendChild(script);
+  }
+
   render() {
     return (
       <div>
-        <div
-          class="fb-share-button"
-          data-href="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4a/Nyan-nyan.jpg/1280px-Nyan-nyan.jpg"
-          data-layout="button"
-          data-size="large"
-        >
-          <a
-            target="_blank"
-            href="https://www.facebook.com/sharer/sharer.php?u=https%3A%2F%2Fupload.wikimedia.org%2Fwikipedia%2Fcommons%2Fthumb%2F4%2F4a%2FNyan-nyan.jpg%2F1280px-Nyan-nyan.jpg&amp;src=sdkpreparse"
-            class="fb-xfbml-parse-ignore"
-          >
-            Share
-          </a>
-        </div>
+        <div class="sharethis-inline-share-buttons" />
       </div>
     );
   }
