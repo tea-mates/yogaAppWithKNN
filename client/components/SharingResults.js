b335f0c3667eaf698c80e950ea3da0234d20c5c8
Lily Zdansky <lzdansky@mail.bradley.edu>
2019-05-08 15:12:27 -0400

    corrected the class name

--- client/components/SharingResults.js
+++ client/components/SharingResults.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-export default class CountdownTimer extends React.Component {
+export default class SharingResults extends React.Component {
   constructor(props) {
     super(props);
   }
