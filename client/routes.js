a38652ff756fc634d0ebd600a14077c60a8f6f1d
Sandra <sandrajanineo@gmail.com>
2019-05-09 12:22:03 -0400

    adjusted the image and paragraph on the about us section

--- client/routes.js
+++ client/routes.js
@@ -3,7 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import AboutUs from './components/AboutUs';
 import SelectPose from './components/SelectPose';
-import SinglePose from './components/SinglePose';
 import Result from './components/ResultPage';
 
 const Routes = () => {
@@ -12,8 +11,6 @@ const Routes = () => {
       <Route exact path="/" component={Home} />
       <Route exact path="/aboutus" component={AboutUs} />
       <Route exact path="/start" component={SelectPose} />
-      <Route exact path="/Namaste" component={SinglePose} />
-      <Route exact path="/Tree" component={SinglePose} />
       <Route exact path="/result" component={Result} />
     </Switch>
   );
