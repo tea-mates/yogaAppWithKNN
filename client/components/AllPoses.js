7fab38b1b07895a2d3ea51c92bfd96a262339ee7
Sandra <sandrajanineo@gmail.com>
2019-05-11 12:58:52 -0400

    connected all poses component to the store

--- client/components/AllPoses.js
+++ client/components/AllPoses.js
@@ -1,66 +1,41 @@
-import React from "react";
-
-const poses = [
-  {
-    name: "Garland",
-    imageUrl: "https://i.imgur.com/16JLzA4.png",
-    highlight: "https://i.imgur.com/bhP3AYN.png"
-  },
-  {
-    name: "Tree",
-    imageUrl:
-      "https://live.staticflickr.com/65535/33935797398_279eda570c_z.jpg",
-    highlight:
-      "https://live.staticflickr.com/65535/33935797398_279eda570c_z.jpg"
-  },
-  {
-    name: "Half Moon",
-    imageUrl:
-      "https://live.staticflickr.com/65535/47812849101_0fba3078a6_z.jpg",
-    highlight:
-      "https://live.staticflickr.com/65535/47812849101_0fba3078a6_z.jpg"
-  },
-  {
-    name: "Half Moon",
-    imageUrl:
-      "https://st4.depositphotos.com/5130171/19866/v/1600/depositphotos_198661934-stock-illustration-child-doing-yoga-half-moon.jpg",
-    highlight:
-      "https://st4.depositphotos.com/5130171/19866/v/1600/depositphotos_198661934-stock-illustration-child-doing-yoga-half-moon.jpg"
-  }
-];
+mport React from 'react';
+import { connect } from 'react-redux';
 
 class AllPoses extends React.Component {
-  constructor(props, context) {
-    super(props, context);
-    this.state = {
-      poses: poses
-    };
+  constructor() {
+    super();
   }
 
   render() {
     return (
       <div className="allPoseImages">
         <div className="poseContainer">
-          <img id="pose0" className="posesImage" src={poses[0].imageUrl} />
-          <img />
-          <img />
-          <img />
-
-          <img id="pose1" className="posesImage" src={poses[1].imageUrl} />
+          <img className="posesImage" src={this.props.poses[0].imageUrl} />
+          <h1>{this.props.poses[0].name}</h1>
+          <div />
+          <div />
+          <div />
+          <img className="posesImage" src={this.props.poses[1].imageUrl} />
+          <h1>{this.props.poses[1].name}</h1>
         </div>
         <br />
         <br />
         <div className="poseContainer2">
-          <img id="pose2" className="posesImage" src={poses[2].imageUrl} />
-          <img />
-          <img />
-          <img />
-
-          <img id="pose4" className="posesImage" src={poses[3].imageUrl} />
+          <div />
+          <div />
+          <div />
+          <img className="posesImage" src={this.props.poses[3].imageUrl} />
+          <h1>{this.props.poses[3].name}</h1>
+          <img className="posesImage" src={this.props.poses[4].imageUrl} />
+          <h1>{this.props.poses[4].name}</h1>
         </div>
       </div>
     );
   }
 }
 
-export default AllPoses;
+const mapState = state => ( {
+  poses: state.poses,
+} );
+
+export default connect( mapState )( AllPoses );
