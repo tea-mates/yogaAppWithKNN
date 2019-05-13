7fb9eafe6abde967605939717e5ebcf89b4567ac
Janavi Anand <janavianand@yahoo.com>
2019-05-13 17:35:03 -0400

    fixed redundant data

--- client/store/index.js
+++ client/store/index.js
@@ -3,16 +3,15 @@ import { createStore,combineReducers,applyMiddleware } from "redux";
 import thunkMiddleware from "redux-thunk";
 import {composeWithDevTools} from 'redux-devtools-extension'
 import resultReducer from './trainer'
+import gameReducer from './game';
 
-const reducer = combineReducers({resultReducer})
+const reducer = combineReducers({resultReducer,gameReducer})
 
 const middleware = composeWithDevTools(
   applyMiddleware(thunkMiddleware, createLogger({collapsed: true}))
 )
 
 const store = createStore(reducer, middleware);
-
-
 
 export default store;
 
