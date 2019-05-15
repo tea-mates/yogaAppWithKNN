9120d41fdddba9548765cbd193d37edc751478a7
Priyanka Garg <you@example.com>
2019-05-15 10:53:52 -0400

    centered camera

--- client/components/trainingSinglePose.js
+++ client/components/trainingSinglePose.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import CountdownTimer from './CountdownTimer';
 import Camera from './Camera';
-import {connect} from 'react-redux'
+import { connect } from 'react-redux';
 import store from '../store';
-import {reset} from '../store/trainer'
-import ResultPage from './ResultPage'
+import { reset } from '../store/trainer';
+import ResultPage from './ResultPage';
 
 class TrainingSinglePose extends React.Component {
   constructor(props) {
@@ -17,7 +17,7 @@ class TrainingSinglePose extends React.Component {
   }
 
   componentDidMount() {
-    store.dispatch(reset())
+    store.dispatch(reset());
     setTimeout(this.displayCamera, 8000);
   }
 
@@ -28,40 +28,51 @@ class TrainingSinglePose extends React.Component {
   render() {
     return (
       <div>
-        {!this.props.stop ?
-        <div>
-          {!this.state.loadCamera ?
-          <div className="countdownDiv">
-            <div>
-              <h1>Get ready!</h1>
-              <CountdownTimer />
-            </div>
-          </div> :
-          <div className="cameraDiv">
-            {stop = null}
-            <Camera poseName={this.props.match.params.poseName} />
-          </div>}
-        </div> :
-        <div>
-          <p>pose name : {this.props.pose}</p>
-          {this.props.score > 0 ?
+        {!this.props.stop ? (
           <div>
-            <div>
-              <p>Score : {parseInt((1-this.props.score)*100)}%</p>
-            </div>
-            <div>
-              <ResultPage percentage = {parseInt((1-this.props.score)*100)}/>
-            </div>
-          </div> :
+            {!this.state.loadCamera ? (
+              <div className="countdownDiv">
+                <div>
+                  <h1>Get ready!</h1>
+                  <CountdownTimer />
+                </div>
+              </div>
+            ) : (
+              <div>
+                {(stop = null)}
+                <Camera
+                  className="trainingCamera"
+                  poseName={this.props.match.params.poseName}
+                />
+              </div>
+            )}
+          </div>
+        ) : (
           <div>
-            <div>
-              <p>Score : 0</p>
-            </div>
-            <div>
-              <ResultPage percentage = {0}/>
-            </div>
-          </div>}
-        </div>}
+            <p>pose name : {this.props.pose}</p>
+            {this.props.score > 0 ? (
+              <div>
+                <div>
+                  <p>Score : {parseInt((1 - this.props.score) * 100)}%</p>
+                </div>
+                <div>
+                  <ResultPage
+                    percentage={parseInt((1 - this.props.score) * 100)}
+                  />
+                </div>
+              </div>
+            ) : (
+              <div>
+                <div>
+                  <p>Score : 0</p>
+                </div>
+                <div>
+                  <ResultPage percentage={0} />
+                </div>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     );
   }
@@ -70,7 +81,7 @@ class TrainingSinglePose extends React.Component {
 const mapState = state => ({
   pose: state.resultReducer.pose,
   score: state.resultReducer.score,
-  stop:state.resultReducer.stop
-})
+  stop: state.resultReducer.stop
+});
 
 export default connect(mapState)(TrainingSinglePose);
